958924591765c523e1e53f07d8041bbb7ce59181
Corpus-PreciousHannah <corpus.precioushannah1@gmail.com>
2025-03-25 09:56:41 +0800

    Add a File

---
+++ main.py
@@ -0,0 +1,19 @@
+#TODO (Name): (Description)
+def get_order_details():
+    pass
+
+#TODO (Name): (Description)
+def get_customer_detsils():
+    pass
+
+#TODO (Name): (Description)
+def calculate_total():
+    pass
+
+#TODO (Name): (Description)
+def display_receipt():
+    pass
+
+#TODO (Name): (Description)
+def main():
+    pass
