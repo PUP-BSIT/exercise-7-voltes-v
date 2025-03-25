b8a17de2f52d4b103a3c1173b8749f436af57c55
Gulles-AltheaShane <gullesalthea00@gmail.com>
2025-03-25 15:33:43 +0800

    add order details

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-#TODO (Name): (Description)
+#TODO (Althea): (Order Details Collection)
+def get_price(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Kindly enter the original price.")
+
+def get_quantity(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Kindly enter your desired quantity.")
+
 def get_order_details():
-    pass
+    order_items = []
+    while True:
+        item = {}
+        item['product_name'] = input("Enter Product Name: ")
+        item['price'] = get_price("Enter Price: ")
+        item['quantity'] = get_quantity("Enter Quantity: ")
+        
+        order_items.append(item)
+        
+        if input("Add another Item? (y/n): ").lower() != 'y':
+            break
+    return order_items
 
 #TODO (Name): (Description)
 def get_customer_detsils():
